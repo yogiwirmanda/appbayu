8edb65d086b9dd9105b04506501cf951f3d44e67
yogiwirmanda <wirmanda@gmail.com>
2022-07-03 13:35:32 +0700

    update schedule to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('whapi:sendMessage')->everyMinute();
+        $schedule->command('whapi:sendMessage')->dailyAt('07:00');
     }
 
     /**
