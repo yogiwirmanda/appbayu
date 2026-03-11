382c5f52361c47cd853632f1941c73d136ebf911
Yogi Wirmanda <wirmanda@gmail.com>
2026-03-11 23:24:47 +0700

    Update AntreanController.php

--- app/Http/Controllers/AntreanController.php
+++ app/Http/Controllers/AntreanController.php
@@ -97,13 +97,13 @@ class AntreanController extends Controller
         } else {
             $modelAntrean = Antrean::create($dataCreate);
 
-            Http::withoutVerifying()
-                ->async()
-                ->post('https://ehealthprc.com/api/api/v1/wa/send-antrean-lab', [
-                    'nama' => $modelAntrean->nama,
-                    'no_hp' => $modelAntrean->nohp,
-                    'tanggal' => $modelAntrean->tanggal
-                ]);
+        Http::withoutVerifying()
+            ->timeout(3)
+            ->post('https://ehealthprc.com/api/api/v1/wa/send-antrean-lab', [
+                'nama' => $modelAntrean->nama,
+                'no_hp' => $modelAntrean->nohp,
+                'tanggal' => $modelAntrean->tanggal
+            ]);
 
         }
 
