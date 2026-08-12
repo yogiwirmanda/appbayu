937705d93a78105b6c9e151c3bd44bf2fd62f0a2
Yogi Wirmanda <wirmanda@gmail.com>
2026-08-12 10:35:36 +0700

    Update PasienController.php

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -1127,6 +1127,7 @@ class PasienController extends Controller
     {
         $modelPasien = Pasien::find($idPasien);
         $modelCeklab = Ceklab::where('id_pasien', $idPasien)
+            ->where('datang', 1)
             ->orderBy('tanggal', 'desc')
             ->first();
 
